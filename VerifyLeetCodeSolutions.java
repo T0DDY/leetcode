1853873a423db3363ffadf8497cb7067e19dc3ca
T0DDY <lyt3721@gmail.com>
2015-12-13 18:51:43 +0800

    test

---
+++ VerifyLeetCodeSolutions.java
@@ -0,0 +1,15 @@
+package net.test.testng.leetcode;
+
+import org.junit.Test;
+
+public class VerifyLeetCodeSolutions {
+
+	public void testSolution() {
+		System.out.println("Hello");
+	}
+
+	@Test
+	public void test() {
+		testSolution();
+	}
+}
